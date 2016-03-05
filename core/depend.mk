f4614c700e743114f6ecf190f97cd4fa0fc735f7
hgvk <hgvk94@gmail.com>
2016-03-06 02:26:42 +0530

    Initial Commit

---
+++ core/depend.mk
@@ -0,0 +1,25 @@
+/home/hari/Documents/DLIS/core/Main.o /home/hari/Documents/DLIS/core/Main.or /home/hari/Documents/DLIS/core/Main.od /home/hari/Documents/DLIS/core/Main.op: /home/hari/Documents/DLIS/core/Main.cc \
+ /home/hari/Documents/DLIS/utils/System.h \
+ /home/hari/Documents/DLIS/mtl/IntTypes.h \
+ /home/hari/Documents/DLIS/utils/ParseUtils.h \
+ /home/hari/Documents/DLIS/utils/Options.h \
+ /home/hari/Documents/DLIS/mtl/Vec.h \
+ /home/hari/Documents/DLIS/mtl/XAlloc.h \
+ /home/hari/Documents/DLIS/core/Dimacs.h \
+ /home/hari/Documents/DLIS/core/SolverTypes.h \
+ /home/hari/Documents/DLIS/mtl/Alg.h /home/hari/Documents/DLIS/mtl/Map.h \
+ /home/hari/Documents/DLIS/mtl/Alloc.h \
+ /home/hari/Documents/DLIS/core/Solver.h \
+ /home/hari/Documents/DLIS/mtl/Heap.h
+/home/hari/Documents/DLIS/core/Solver.o /home/hari/Documents/DLIS/core/Solver.or /home/hari/Documents/DLIS/core/Solver.od /home/hari/Documents/DLIS/core/Solver.op: /home/hari/Documents/DLIS/core/Solver.cc \
+ /home/hari/Documents/DLIS/mtl/Sort.h /home/hari/Documents/DLIS/mtl/Vec.h \
+ /home/hari/Documents/DLIS/mtl/IntTypes.h \
+ /home/hari/Documents/DLIS/mtl/XAlloc.h \
+ /home/hari/Documents/DLIS/core/Solver.h \
+ /home/hari/Documents/DLIS/mtl/Heap.h /home/hari/Documents/DLIS/mtl/Alg.h \
+ /home/hari/Documents/DLIS/utils/Options.h \
+ /home/hari/Documents/DLIS/utils/ParseUtils.h \
+ /home/hari/Documents/DLIS/core/SolverTypes.h \
+ /home/hari/Documents/DLIS/mtl/Map.h \
+ /home/hari/Documents/DLIS/mtl/Alloc.h \
+ /home/hari/Documents/DLIS/utils/System.h
